9120477e34e156278d72265af491711aa2fe0603
harshalitalele <harshalitalele@gmail.com>
2019-05-08 19:30:20 +0530

    puzzle progress

--- LeetCode/Medium/decodeString.js
+++ LeetCode/Medium/decodeString.js
@@ -1,4 +1,4 @@
-var s = "3[a2[bc4[d]]]";
+var s = "10[a2[d]]";
 
 var decodeString = function() {
     var decodedStr = "",
@@ -8,7 +8,7 @@ var decodeString = function() {
     while(i < s.length) {
         var curCh = s[i];
         if(Number(curCh)) {
-            var tempDecode = evalStr(i, s);
+            var tempDecode = evalStr(i, s, "");
             i = tempDecode[0];
             decodedStr += tempDecode[1];
         } else {
@@ -19,18 +19,61 @@ var decodeString = function() {
     console.log(decodedStr);
 }
 
-function evalStr(index, str) {
+function evalStr(index, str, prevStr, cnt) {
     var nextCh = str[index];
-    if(nextCh == ']') {
-        return [index, ''];
-    } else if(Number(nextCh)) {
-        var tempAns = evalStr(index+2, str),
-            str = "";
-        for(var i = 0; i < Number(nextCh); i++) {
-            str += tempAns[1];
+    
+    if(nextCh == undefined) {
+        return prevStr;
+    } else if(!isNaN(Number(nextCh))) {
+        if(Number(prevStr)) {
+            prevStr += nextCh;
+        } else {
+            prevStr = nextCh;
         }
-        return [tempAns[0]+1, str];
+        return evalStr(index+1, str, prevStr);
+    } else if(nextCh == '[') {
+        return evalStr(index+1, str, "", Number(prevStr));
+    } else if(nextCh == ']') {
+        var tempAns = "";
+        for(var i = 0; i < cnt; i++) {
+            tempAns += prevStr;
+        }
+        return evalStr(index+1, str, tempAns);
+    } else {
+        prevStr += nextCh;
+        return evalStr(index+1, str, prevStr, cnt);
     }
-    var evaluation = evalStr(index+1, str);
-    return [evaluation[0], nextCh + evaluation[1]];
+    
+    return prevStr;
 }
+
+var sub = "2[a2[d]]";
+
+function decodeSubStr(str, index, mul) {
+    var subStr = "";
+    if(!mul) {
+        mul = "";
+    }
+    for(var i = index; i < str.length; i++) {
+        var curCh = str[i];
+        if(curCh === '[') {
+            return decodeSubStr(str, i+1, Number(mul));
+        } else if(curCh === ']') {
+            return mulStr(subStr, mul);
+        } else if(!isNaN(Number(curCh))) {
+            mul += curCh;
+        } else {
+            subStr += curCh;
+        }
+    }
+}
+
+function mulStr(str, m) {
+    var ans = "";
+    for(var i = 0; i < m; i++) {
+        ans += str;
+    }
+    return ans;
+}
+
+console.log(decodeSubStr("15[a2[d]]", 0));
