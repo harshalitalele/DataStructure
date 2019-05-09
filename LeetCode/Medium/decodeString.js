f98979d3e93ed5c2af9539d96700128809c76b28
harshalitalele <harshalitalele@gmail.com>
2019-05-09 17:11:05 +0530

    corrected for remaining string

--- LeetCode/Medium/decodeString.js
+++ LeetCode/Medium/decodeString.js
@@ -1,53 +1,10 @@
 var s = "10[a2[d]]";
 
 var decodeString = function() {
-    var decodedStr = "",
-        cnt = 0,
-        repStr = '',
-        i = 0;
-    while(i < s.length) {
-        var curCh = s[i];
-        if(Number(curCh)) {
-            var tempDecode = evalStr(i, s, "");
-            i = tempDecode[0];
-            decodedStr += tempDecode[1];
-        } else {
-            decodedStr += curCh;
-            i++;
-        }
-    }
-    console.log(decodedStr);
+    var decodedStr = "";
+    console.log(decodeSubStr(s, 0));
+    return decodeSubStr(s, 0);
 }
-
-function evalStr(index, str, prevStr, cnt) {
-    var nextCh = str[index];
-    
-    if(nextCh == undefined) {
-        return prevStr;
-    } else if(!isNaN(Number(nextCh))) {
-        if(Number(prevStr)) {
-            prevStr += nextCh;
-        } else {
-            prevStr = nextCh;
-        }
-        return evalStr(index+1, str, prevStr);
-    } else if(nextCh == '[') {
-        return evalStr(index+1, str, "", Number(prevStr));
-    } else if(nextCh == ']') {
-        var tempAns = "";
-        for(var i = 0; i < cnt; i++) {
-            tempAns += prevStr;
-        }
-        return evalStr(index+1, str, tempAns);
-    } else {
-        prevStr += nextCh;
-        return evalStr(index+1, str, prevStr, cnt);
-    }
-    
-    return prevStr;
-}
-
-var sub = "2[a2[d]]";
 
 function decodeSubStr(str, index, mul) {
     var subStr = "";
@@ -59,9 +16,10 @@ function decodeSubStr(str, index, mul) {
         if(curCh === '[') {
             var repStr = decodeSubStr(str, i+1),
                 ans = mulStr(repStr[0], mul);
-            return [subStr + ans, repStr[1]];
+            subStr = subStr + ans;
+            i = repStr[1];
         } else if(curCh === ']') {
-            return [subStr, index];
+            return [subStr, i];
         } else if(!isNaN(Number(curCh))) {
             if(mul === undefined) {
                 mul = curCh;
@@ -72,6 +30,7 @@ function decodeSubStr(str, index, mul) {
             subStr += curCh;
         }
     }
+    return subStr;
 }
 
 function mulStr(str, m) {
@@ -81,5 +40,3 @@ function mulStr(str, m) {
     }
     return ans;
 }
-
-console.log(decodeSubStr("[a12[b]]", 0, 10));
